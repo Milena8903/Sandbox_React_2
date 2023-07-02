88db6c87065cf385ed4effc79739c6bfb0a88467
Milena8903 <milena_8903@hotmail.com>
2023-07-02 18:16:08 -0500

    Vr. 0.0.0.2

--- front_react_project/src/pages/patientFormPage.tsx
+++ front_react_project/src/pages/patientFormPage.tsx
@@ -7,6 +7,10 @@ export function PatientForm (){
         try{
             const nuevoPaciente = {
                 cedula:
+                nombre:
+                apellido:
+                fecha:
+                telefono:
             }
         }
     }
